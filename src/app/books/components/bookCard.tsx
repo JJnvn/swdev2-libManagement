41aee8fd38e0e6ff1b7ce1694ed877b2f65d150e
JJnvn <raptor.jj05@gmail.com>
2025-11-14 22:01:40 +0700

    fix: delete book for admin

--- src/app/books/components/bookCard.tsx
+++ src/app/books/components/bookCard.tsx
@@ -1,18 +1,38 @@
 "use client";
-import Link from "next/link";
 import { useAuth } from "@/context/AuthContext";
+import api from "@/lib/api";
+import Link from "next/link";
 
-export default function BookCard({ book }: { book: any }) {
+export default function BookCard({ book, mutateBooks }: any) {
     const { user } = useAuth();
+
+    const deleteBook = async () => {
+        if (!confirm("Delete this book?")) return;
+
+        try {
+            await api.delete(`/api/v1/books/${book.id}`);
+
+            // refresh cached list
+            mutateBooks();
+
+            alert("Book deleted");
+        } catch (err) {
+            console.error(err);
+            alert("Failed");
+        }
+    };
 
     return (
         <div className="bg-white shadow rounded-xl p-4 flex flex-col justify-between hover:shadow-lg transition">
             <div>
-                <img
-                    src={book.coverPicture}
-                    alt={book.title}
-                    className="w-full h-48 object-cover rounded-lg mb-3"
-                />
+                <Link href={`/books/${book.id}`}>
+                    <img
+                        src={book.coverPicture}
+                        alt={book.title}
+                        className="w-full h-48 object-cover rounded-lg mb-3 cursor-pointer"
+                    />
+                </Link>
+
                 <h3 className="text-lg font-semibold text-gray-800">
                     {book.title}
                 </h3>
@@ -27,13 +47,14 @@ export default function BookCard({ book }: { book: any }) {
                 >
                     View
                 </Link>
+
                 {user?.role === "admin" && (
-                    <Link
-                        href={`/books/${book.id}/delete`}
-                        className="text-red-500 hover:text-red-700 font-medium"
+                    <button
+                        onClick={deleteBook}
+                        className="text-red-500 hover:text-red-700 font-medium cursor-pointer"
                     >
                         Delete
-                    </Link>
+                    </button>
                 )}
             </div>
         </div>
